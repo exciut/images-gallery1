1803f8c026da533456a77ffde6531a4189d507af
exciut <egor.ilinykh1337@gmail.com>
2023-05-05 21:08:30 +1000

    introduce axios and rewrite new-images fetch call

--- api/main.py
+++ api/main.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 from mongo_client import mongo_client
 
-gallery = mongo_client.gallery
-images_collection = gallery.images
+gallery = mongo_client.gallery  # created 'gallery' database in MongoDB
+images_collection = gallery.images  # Created 'images_collection' in database
 
 load_dotenv(dotenv_path="./.env.local")
 print(os.environ.get("UNSPLASH_KEY", ""))
@@ -42,15 +42,27 @@ def new_image():
 def images():
     if request.method == "GET":
         # read images from database using .find in images-collection and return Json {} in database
-        images = images_collection.find({})
-        return jsonify([img for img in images])
+        images = images_collection.find(
+            {}
+        )  # this find operation refers to all data in the images collection
+        return jsonify(
+            [img for img in images]
+        )  # return Json object, that contains array of objects, where each object= separate image in "images_collection" collection
     if request.method == "POST":
         # save image to the database
-        image = request.get_json()
-        image["_id"] = image.get("id")
-        result = images_collection.insert_one(image)
+        image = (
+            request.get_json()
+        )  # we use data, that sent from client in Json format, using get method
+        image["_id"] = image.get(
+            "id"
+        )  # here we are able to modify such object(we created new field: "_id")
+        result = images_collection.insert_one(
+            image
+        )  # We perform insert_one operation, and insert one new data in our collection
         inserted_id = result.inserted_id
-        return {"inserted_id": inserted_id}
+        return {
+            "inserted_id": inserted_id
+        }  # flask automatically convert such dictionary to JSON format
 
 
 if __name__ == "__main__":
